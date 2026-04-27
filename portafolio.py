b789d18ce8cbead75ff8c5416ed1383e99f2760e
jzapgal <jzapgal@gmail.com>
2026-04-26 21:26:57 -0500

    agregar def vender

--- portafolio.py
+++ portafolio.py
@@ -18,10 +18,8 @@ class PortafolioReal(PortafolioBase):
         precio_mercado = activo.get_precio_actual()
         precio_min = precio_mercado * 0.98
         precio_max = precio_mercado * 1.02
-
         if precio < precio_min or precio > precio_max:
             raise ValueError("Precio fuera del rango permitido para la jornada")
-
         costo_total = precio * cantidad
         if costo_total > self.capital_disponible:
             raise ValueError("Capital insuficiente")
@@ -31,10 +29,8 @@ class PortafolioReal(PortafolioBase):
         precio_mercado = activo.get_precio_actual()
         precio_min = precio_mercado * 0.98
         precio_max = precio_mercado * 1.02
-
         if precio < precio_min or precio > precio_max:
             raise ValueError("Precio fuera del rango permitido para la jornada")
-
         if self.cantidades[ticker] < cantidad:
             raise ValueError("No tienes suficientes unidades para vender")
 
@@ -48,4 +44,16 @@ class PortafolioReal(PortafolioBase):
         t = Transaccion("compra", ticker, cantidad, precio, comision, date.today())
         self.historial.agregar_al_final(t)
         self.compras.push(t)
+        return t
+
+    def vender(self, ticker: str, cantidad: float) -> Transaccion:
+        precio = self.activos[ticker].get_precio_actual()
+        self._validar_venta(ticker, cantidad, precio)
+        comision = precio * cantidad * 0.005
+        costo_total = precio * cantidad + comision
+        self.capital_disponible += costo_total
+        self.cantidades[ticker] = self.cantidades.get(ticker, 0) - cantidad
+        t = Transaccion("venta", ticker, cantidad, precio, comision, date.today())
+        self.historial.agregar_al_final(t)
+        self.ventas.encolar(t)
         return t
